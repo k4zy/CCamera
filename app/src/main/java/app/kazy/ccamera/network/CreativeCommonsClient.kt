51a2eae133f082c60b08a3bd67e95f5291333439
kazuki-yoshida <kazuki-yoshida@cookpad.com>
2020-12-30 19:48:58 +0900

    Use BuildConfig

--- app/src/main/java/app/kazy/ccamera/network/CreativeCommonsClient.kt
+++ app/src/main/java/app/kazy/ccamera/network/CreativeCommonsClient.kt
@@ -1,11 +1,12 @@
 package app.kazy.ccamera.network
 
+import app.kazy.ccamera.BuildConfig
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface CreativeCommonsClient {
-    @Headers("Authorization: Bearer DLBYIcfnKfolaXKcmMC8RIDCavc2hW")
+    @Headers("Authorization: Bearer ${BuildConfig.API_KEY}")
     @GET("/v1/images")
     suspend fun searchImages(@Query("q") searchWord: String): SearchResponse
 }
