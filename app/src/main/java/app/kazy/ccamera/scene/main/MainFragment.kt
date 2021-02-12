bc6fdfb1045e5281241706603b3cddd76f14c8a3
kazuki-yoshida <kazuki-yoshida@cookpad.com>
2021-02-12 20:56:30 +0900

    Refactoring

--- app/src/main/java/app/kazy/ccamera/scene/main/MainFragment.kt
+++ app/src/main/java/app/kazy/ccamera/scene/main/MainFragment.kt
@@ -1,79 +1,19 @@
 package app.kazy.ccamera.scene.main
 
-import android.content.Intent
-import android.graphics.Bitmap
-import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import app.kazy.ccamera.databinding.FragmentMainBinding
-import com.bumptech.glide.Glide
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import java.io.File
-import java.io.FileOutputStream
-import java.io.OutputStream
 
 class MainFragment : Fragment() {
-    @Suppress("BlockingMethodInNonBlockingContext")
-    private val adapter = ImageAdapter { image ->
-        GlobalScope.launch {
-            Glide.with(requireContext())
-                .asBitmap()
-                .load(image.url)
-                .submit()
-                .get()
-                .let {
-                    saveImage(it)
-                }
-        }
-    }
-
-    private fun saveImage(image: Bitmap): String? {
-        var savedImagePath: String? = null
-        val imageFileName = "JPEG_" + "FILE_NAME" + ".jpg"
-        val storageDir = File(
-            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
-                .toString() + "/YOUR_FOLDER_NAME"
-        )
-        var success = true
-        if (!storageDir.exists()) {
-            success = storageDir.mkdirs()
-        }
-        if (success) {
-            val imageFile = File(storageDir, imageFileName)
-            savedImagePath = imageFile.getAbsolutePath()
-            try {
-                val fOut: OutputStream = FileOutputStream(imageFile)
-                image.compress(Bitmap.CompressFormat.JPEG, 100, fOut)
-                fOut.close()
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-
-            galleryAddPic(savedImagePath)
-        }
-        return savedImagePath
-    }
-
-    private fun galleryAddPic(imagePath: String?) {
-        imagePath?.let { path ->
-            val mediaScanIntent = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE)
-            val f = File(path)
-            val contentUri: Uri = Uri.fromFile(f)
-            mediaScanIntent.data = contentUri
-            requireActivity().sendBroadcast(mediaScanIntent)
-        }
-    }
-
     private val viewModel by viewModels<MainViewModel>()
     private lateinit var binding: FragmentMainBinding
+    private val adapter = ImageAdapter { image ->
+        viewModel.saveImage(requireContext(), image.url)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
--- app/src/main/java/app/kazy/ccamera/scene/main/MainViewModel.kt
+++ app/src/main/java/app/kazy/ccamera/scene/main/MainViewModel.kt
@@ -1,15 +1,27 @@
 package app.kazy.ccamera.scene.main
 
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Environment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import app.kazy.ccamera.network.CreativeCommonsClient
+import com.bumptech.glide.Glide
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import timber.log.Timber
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
+import java.util.concurrent.Future
 
 class MainViewModel : ViewModel() {
     private val client: CreativeCommonsClient
@@ -31,6 +43,62 @@ class MainViewModel : ViewModel() {
         viewModelScope.launch {
             val searchResponse = client.searchImages(word)
             _images.postValue(searchResponse.results.map { it.convert() })
+        }
+    }
+
+    @Suppress("BlockingMethodInNonBlockingContext")
+    private suspend fun <T> Future<T>.wait(): T {
+        while (!isDone)
+            delay(1)
+        return get()
+    }
+
+    fun saveImage(context: Context, uri: Uri) = viewModelScope.launch {
+        val bitmap = loadBitmap(context, uri)
+        @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+        val fileName = File(uri.path).name
+        saveImage(context, fileName, bitmap)
+    }
+
+    private suspend fun loadBitmap(context: Context, uri: Uri) = Glide.with(context)
+        .asBitmap()
+        .load(uri)
+        .submit()
+        .wait()
+
+    private fun saveImage(context: Context, fileName: String, image: Bitmap): String? {
+        var savedImagePath: String? = null
+        val imageFileName = "JPEG_$fileName.jpg"
+        val storageDir = File(
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+                .toString() + "/CCamera"
+        )
+        var success = true
+        if (!storageDir.exists()) {
+            success = storageDir.mkdirs()
+        }
+        if (success) {
+            val imageFile = File(storageDir, imageFileName)
+            savedImagePath = imageFile.absolutePath
+            try {
+                val fOut: OutputStream = FileOutputStream(imageFile)
+                image.compress(Bitmap.CompressFormat.JPEG, 100, fOut)
+                fOut.close()
+            } catch (e: Exception) {
+                Timber.e(e)
+            }
+            galleryAddPic(context, savedImagePath)
+        }
+        return savedImagePath
+    }
+
+    private fun galleryAddPic(context: Context, imagePath: String?) {
+        imagePath?.let { path ->
+            val mediaScanIntent = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE)
+            val f = File(path)
+            val contentUri: Uri = Uri.fromFile(f)
+            mediaScanIntent.data = contentUri
+            context.sendBroadcast(mediaScanIntent)
         }
     }
 }
